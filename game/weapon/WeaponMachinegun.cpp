c7162ae3f50d8958051ae9892d0c52b539b9c3ca
Adreany Sihombing <aksihombing05@gmail.com>
2024-02-10 21:31:55 -0500

    Updated and fixed errors after building original sdk in VS

--- game/weapon/WeaponMachinegun.cpp
+++ game/weapon/WeaponMachinegun.cpp
@@ -228,11 +228,11 @@ stateResult_t rvWeaponMachinegun::State_Fire ( const stateParms_t& parms ) {
 		case STAGE_INIT:
 			if ( wsfl.zoom ) {
 				nextAttackTime = gameLocal.time + (altFireRate * owner->PowerUpModifier ( PMOD_FIRERATE ));
-				Attack ( true, 1, spreadZoom, 0, 1.0f );
+				Attack ( true, 1, spreadZoom * 2, 0, 0.7f );
 				fireHeld = true;
 			} else {
 				nextAttackTime = gameLocal.time + (fireRate * owner->PowerUpModifier ( PMOD_FIRERATE ));
-				Attack ( false, 1, spread, 0, 1.0f );
+				Attack ( false, 1, spread * 2, 0, 0.7f );
 			}
 			PlayAnim ( ANIMCHANNEL_ALL, "fire", 0 );	
 			return SRESULT_STAGE ( STAGE_WAIT );
